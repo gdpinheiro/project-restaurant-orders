bf2bd65864591667d319b96af027db85e9966443
black <gikigiio@gmail.com>
2022-09-23 13:35:01 -0300

    Req 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,52 @@
+from collections import Counter
+
+
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        customer_orders = []
+        for order in self.orders:
+            if order[0] == customer:
+                customer_orders.append(order[1])
+        return Counter(customer_orders).most_common()[0][0]
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        dishes = set()
+        customer_orders = set()
+        for order in self.orders:
+            if order[1] not in dishes:
+                dishes.add(order[1])
+            if order[0] == customer:
+                customer_orders.add(order[1])
+        return dishes.difference(customer_orders)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        days = set()
+        customer_days = set()
+        for order in self.orders:
+            if order[2] not in days:
+                days.add(order[2])
+            if order[0] == customer:
+                customer_days.add(order[2])
+        return days.difference(customer_days)
 
     def get_busiest_day(self):
-        pass
+        customer_orders = []
+        for order in self.orders:
+            customer_orders.append(order[2])
+        return Counter(customer_orders).most_common()[0][0]
 
     def get_least_busy_day(self):
-        pass
+        customer_orders = []
+        for order in self.orders:
+            customer_orders.append(order[2])
+        counted_days = Counter(customer_orders).most_common()
+        return counted_days[len(counted_days) - 1][0]
